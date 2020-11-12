be316b85c25a39927c740662bc01bd82290eda94
ibizaSunrise <milalemekh@gmail.com>
2020-11-12 12:28:06 +0100

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-const modal = $.modal()
+const modal = $.modal({
+    title: 'My Modal',
+    closable: true,
+    content: `
+    <p>Lorem ipsum dolor sit.</p>
+    `,
+    width: '400px',
+    footerButtons: [
+        {
+            text: 'Ok',
+            type: 'primary',
+            handler(){
+                console.log('Primary btn clicked')
+            }
+        },
+        {
+            text: 'Cansel',
+            type: 'danger',
+            handler(){
+                console.log('Danger btn clicked')
+            }
+        }
+    ]
+})
